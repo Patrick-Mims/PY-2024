88fa761b8fb902959ac6b3f67aacfc8dca24df96
patrick <patrickkevinmims@gmail.com>
2024-03-26 12:20:49 -0700

    Exercise 1

---
+++ BAN-601/Module-1/main.py
@@ -0,0 +1,17 @@
+# Write a function sum_product(a,b) that returns both the sum and the product
+# of two numbers, the result should be returned as a tuple (sum, product).
+# #
+
+def sum_product(a,b):
+    a = (a + b)
+    p = (a * b)
+    return a, p
+
+print(sum_product(5,30))
+
+# Using sorted() function, write a function second_largest(num) that returns the
+# second largest number in a list of numbers.
+# For simplicity, you can assume the list has at least two distinct numbers.
+# #
+
+
